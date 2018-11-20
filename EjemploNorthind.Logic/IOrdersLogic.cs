cef43ddfe7dac35e1f84e2f3e4f8c9961337005f
Daniel Ubilla Acuña <dubillaa477@ulacit.ed.cr>
2018-11-19 19:58:22 -0600

    a

---
+++ EjemploNorthind.Logic/IOrdersLogic.cs
@@ -0,0 +1,16 @@
+﻿using EjemploNorthwind.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploNorthind.Logic
+{
+    interface IOrdersLogic
+    {
+        Orders GetById(int Id);
+        OrderLine EditLine(int LineId, int Quantity);   
+
+    }
+}
